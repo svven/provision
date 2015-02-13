383017f887069d219c617edb8a1b0014c6b7546f
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-02-13 21:04:18 +0100

    Common scripts moved to bootstrap

--- provision.sh
+++ provision.sh
@@ -1,23 +1,50 @@
 #!/bin/bash
 echo "
-############################################################
+##############################################################################
 ## Provision (via Vagrant or EC2 User Data)
-############################################################
-"
+## User: $USER (e.g. root, vagrant, ubuntu)
+##############################################################################"
 
-ADMIN=ubuntu; USER=svven; COMPONENT=redis;
-PRIVATE_KEY=https://dl.dropboxusercontent.com/u/134594/svven/svven_rsa
-PUBLIC_KEY=https://dl.dropboxusercontent.com/u/134594/svven/svven_rsa.pub
+COMPONENT=${1:-all} # (e.g.: postgre, redis, nginx, web, poller, summarizer)
 
-PROVISION=https://raw.githubusercontent.com/svven/provision/master/provision.sh
-BOOTSTRAP_GIT=git@bitbucket.org:svven/bootstrap.git
+NEW_USER=svven
+PRIVATE_KEY=https://www.dropbox.com/s/5le6maruiold9lc/svven_rsa?dl=1
+# PUBLIC_KEY=https://www.dropbox.com/s/5le6maruiold9lc/svven_rsa.pub?dl=1
 
-## Get bootstrap scripts
-curl $PROVISION | sudo -u $ADMIN -H bash -s $BOOTSTRAP_GIT $PRIVATE_KEY
+BOOTSTRAP_GIT_REPO=https://github.com/svven/bootstrap.git
+PROVISION_GIT_REPO=git@bitbucket.org:svven/provision.git
+
+## Locale fix
+## http://www.pixelninja.me/how-to-fix-invalid-locale-setting-in-ubuntu-14-04-in-the-cloud/
+sudo locale-gen en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+echo "LC_ALL=\"en_US.UTF-8\"" | sudo tee -a /etc/environment /etc/default/locale
+echo "LANGUAGE=\"en_US.UTF-8\"" | sudo tee -a /etc/environment /etc/default/locale
+
+## Update and upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
+
+## Major requirements
+sudo apt-get install -y build-essential git
 
 ## Go home
-cd /home/$ADMIN
+cd $HOME # /home/$USER
+
+## Get public bootstrap scripts
+if [ ! -d bootstrap ]; then
+    git clone $BOOTSTRAP_GIT_REPO
+fi
+
+## Add new user and set SSH keys
+sudo -u $USER -H bash bootstrap/adduser.sh $NEW_USER
+sudo -u $NEW_USER -H bash bootstrap/setssh.sh $PRIVATE_KEY $PUBLIC_KEY
+
+## Get private provision scripts
+if [ ! -d provision ]; then
+    sudo -u $USER -H bash bootstrap/gitclone.sh $PROVISION_GIT_REPO $PRIVATE_KEY
+fi
 
 ## Install the component
-sudo -u $ADMIN -H bash bootstrap/install.sh \
-    $ADMIN $USER $COMPONENT $PRIVATE_KEY $PUBLIC_KEY
+sudo -u $NEW_USER -H bash provision/install.sh $COMPONENT
