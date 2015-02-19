b6814aa036e071f6f7ee0ce36c8358eb7a4cc3e0
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-02-19 15:43:34 +0000

    Postgre samenet trust

--- servers/postgre.sh
+++ servers/postgre.sh
@@ -17,8 +17,12 @@ sudo apt-get install -y $POSTGRE_VER postgresql-contrib
 
 ## Configure
 sudo cp $DIR/conf/postgresql.conf /etc/postgresql/$VER/main/postgresql.conf
+sudo cp $DIR/conf/pg_hba.conf /etc/postgresql/$VER/main/pg_hba.conf
+echo "
+host $USER $USER samenet trust" | sudo tee -a /etc/postgresql/$VER/main/pg_hba.conf
+sudo service postgresql reload
 
 ## Database
-sudo -u postgres createuser $USER ## dropuser $USER
-sudo -u postgres createdb --owner $USER $USER ## dropdb $USER
+sudo -u postgres createuser -D -A $USER ## dropuser $USER
+sudo -u postgres createdb -O $USER $USER ## dropdb $USER
 sudo -u $USER psql -c "grant all privileges on database $USER to $USER;"
