bb4f29b2521e7f9216fd4ca96d82f776fae7812c
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-02-19 18:43:30 +0000

    Cleaner provision

--- provision.sh
+++ provision.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-USER=ubuntu; HOME=/home/ubuntu
 echo "
 ##############################################################################
 ## Provision
@@ -12,47 +11,34 @@ if [[ $COMPONENT == "all" ]]; then
     COMPONENT=$ALL
 fi
 
-NEW_USER=svven
+USER=ubuntu; NEW_USER=svven
 PRIVATE_KEY=https://www.dropbox.com/s/5le6maruiold9lc/svven_rsa?dl=1
 # PUBLIC_KEY=https://www.dropbox.com/s/5le6maruiold9lc/svven_rsa.pub?dl=1
 
 SYSADMIN_GIT_REPO=https://bitbucket.org/svven/sysadmin.git
 PROVISION_GIT_REPO=git@bitbucket.org:svven/provision.git
 
-## Fix locale before
-curl -L $SYSADMIN_GIT_REPO/raw/master/fixlocale.sh | bash
-
-## Update and upgrade
-sudo apt-get update
-sudo apt-get upgrade -y
-
-## Major requirements
-sudo apt-get install -y build-essential git
+## Update first
+curl -L $SYSADMIN_GIT_REPO/raw/master/update.sh | bash
 
 ## Go home
-cd $HOME # /home/$USER
+cd /home/$USER
 
-cat << "EOF" >> sudo -u $USER -H bash
-## Get public sysadmin scripts
+## Get sysadmin and provision scripts
+sudo -u $USER -H bash -c "
 if [ ! -d sysadmin ]; then
     git clone $SYSADMIN_GIT_REPO
 fi
-
-## Get private provision scripts
 if [ ! -d provision ]; then
     bash sysadmin/setssh.sh $PRIVATE_KEY
     source .bash_profile
     git clone $PROVISION_GIT_REPO
-fi
+fi"
 
-## Add new user
+## Add the new user
 bash sysadmin/adduser.sh $NEW_USER
-EOF
 
-cat << "EOF" >> sudo -u $NEW_USER -H bash
-## Set SSH keys
+## Set SSH keys and install the component
+sudo -u $NEW_USER -H bash -c "
 bash sysadmin/setssh.sh $PRIVATE_KEY $PUBLIC_KEY
-
-## Install the component
-# bash provision/install.sh "$COMPONENT"
-EOF
+bash provision/install.sh '$COMPONENT'"
