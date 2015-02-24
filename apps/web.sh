04e8ad2be3b3589b355f443cc75f86ca58fc1e7f
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-02-24 11:59:18 +0100

    Configure web app

--- apps/web.sh
+++ apps/web.sh
@@ -6,8 +6,8 @@ echo "
 ##############################################################################
 "
 
-REPO=web
-GIT_REPO=git@bitbucket.org:svven/$REPO.git
+APP=web
+APP_GIT_REPO=git@bitbucket.org:svven/$APP.git
 DIR=$( cd "$( dirname "$0" )/.." && pwd )
 
 ## Go home
@@ -17,10 +17,10 @@ cd $HOME # /home/$USER
 source .bash_profile
 
 ## Install
-if [ ! -d $REPO ]; then
-    git clone $GIT_REPO
+if [ ! -d $APP ]; then
+    git clone $APP_GIT_REPO
 fi
-cd $REPO
+cd $APP
 
 ## Activate virtualenv
 if [ ! -d env ]; then
@@ -31,12 +31,31 @@ source env/bin/activate
 ## Requirements
 pip install -r requirements.txt
 
+## Configure supervisor
+GUNICORN=$(which gunicorn)
+DIRECTORY=$PWD
+
+LOG_FOLDER=/var/log/$APP
+sudo mkdir -p $LOG_FOLDER && sudo chown $USER $LOG_FOLDER
+
+ENVIRONMENT=""
+while read line; do
+    ENVIRONMENT=$ENVIRONMENT$line,
+done < $HOME/.env
+
+eval "echo \"$(< $DIR/conf/supervisor/$APP.conf)\"" | sudo tee /etc/supervisor/conf.d/$APP.conf
+
 # ## Start gunicorn
 # gunicorn manage:app -b unix:/tmp/gunicorn.sock -w 4 -D
 
-# ## Configure nginx
-# STATIC_ROOT=$REPO/web/static/
-# eval "echo \"$(< $DIR/conf/nginx/web.conf)\"" | sudo tee /etc/nginx/conf.d/web.conf
+## Configure nginx
+STATIC_ROOT=$PWD/$APP/static/
 
-# ## Start service
-# sudo service nginx restart
+eval "echo \"$(< $DIR/conf/nginx/$APP.conf)\"" | sudo tee /etc/nginx/conf.d/$APP.conf
+
+# ## Start services
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start $APP
+
+sudo service nginx restart
