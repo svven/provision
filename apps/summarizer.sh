230960056ed1d97e6132d5c6f1b6f503b3ccbda1
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-02-26 21:43:19 +0100

    Install PhantomJS for summarizer

--- apps/summarizer.sh
+++ apps/summarizer.sh
@@ -10,6 +10,15 @@ APP=summarizer
 APP_GIT_REPO=git@bitbucket.org:svven/$APP.git
 DIR=$( cd "$( dirname "$0" )/.." && pwd )
 
+## PhantomJS
+PHANTOMJS_VER="phantomjs-1.9.8-linux-x86_64"
+if [ ! -f /usr/local/bin/phantomjs ]; then
+    cd /usr/local/share
+    sudo wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOMJS_VER.tar.bz2
+    sudo tar xvjf $PHANTOMJS_VER.tar.bz2
+    sudo ln -sf /usr/local/share/$PHANTOMJS_VER/bin/phantomjs /usr/local/bin/phantomjs
+fi
+
 ## Go home
 cd $HOME # /home/$USER
 
