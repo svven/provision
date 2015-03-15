e90aa498c9ecd2f63e8451e1f6b837dbaf55c72e
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-03-15 17:03:22 +0100

    Postgre backup

---
+++ jobs/postgre/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "
+##############################################################################
+## Postgre backup
+## Date: `date`
+##############################################################################
+"
+
+BUCKET=svven-backups
+EXPORTFILE=dump_`date +%u`.pdb # day of week
+COMPRESSEDFILE=$EXPORTFILE.tgz
+
+pg_dump -f ./$EXPORTFILE -c svven
+tar -czf $COMPRESSEDFILE $EXPORTFILE
+
+s3cmd put ./$COMPRESSEDFILE s3://$BUCKET
+rm $COMPRESSEDFILE $EXPORTFILE
