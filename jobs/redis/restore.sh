93583b54531b36d1edaebbf6bf389972d821594b
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2016-01-12 15:19:02 +0000

    Fix redis restore job.

--- jobs/redis/restore.sh
+++ jobs/redis/restore.sh
@@ -14,6 +14,6 @@ s3cmd get s3://$BUCKET/$COMPRESSEDFILE $COMPRESSEDFILE
 tar -xzf $COMPRESSEDFILE $IMPORTFILE
 
 rm $COMPRESSEDFILE
-sudo mv $EXPORTFILE /var/lib/redis/dump.rdb
+sudo mv $IMPORTFILE /var/lib/redis/dump.rdb
 sudo chown redis:redis /var/lib/redis/dump.rdb
 sudo service redis-server restart
